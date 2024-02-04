17eddcf97523c5c8b24f0331f08a7b4e7f5b7d97
spacecreator8 <spcc888@ya.ru>
2024-02-05 04:53:53 +0700

    Сделал таблицу результатов

--- scripts/test.js
+++ scripts/test.js
@@ -3,6 +3,7 @@ let plate = document.querySelector('.plate');
 
 let startGameFlag = false;
 let pauseFlag = false;
+let idForPause;
 
 let positivityX;
 let directionX, directionY;
@@ -22,15 +23,31 @@ let ballCenter = {
     x : ballXCord + Math.trunc(ball.clientWidth / 2),
     y : ballYCord + Math.trunc(ball.clientHeight / 2),
 }
+let arr1, arr2, arr3, arr4;
 
 
-let playersDataBase = {};
+let playersDataBase = {
+    '@@@ANIGIL9TOP@@@':990,
+    '123':810,
+    'VitekVasyan':700,
+    'DOBPb|H9|':9999,
+    'aaa':1320,
+    'sss':1000,
+    'Igor':500,
+    'Бабуля':1100,
+    'Пращур':800,
+    'Олежа BELGORODSKIY':10,
+}
 let playername;
 let preventScores;
-let actualScores;
+let actualScr = document.querySelector('.actualScr').innerHTML;
+let actualScores = document.querySelector('.actualScores').innerHTML;
 let screen1;
+let screen2 = document.querySelector('.main_box');
+let lifes = document.querySelector('.lifes').innerHTML;
 
-let lifes = document.querySelector('.lifes');
+let endGameFlag=false;
+let winnersTable;
 
 function plateMotion(event){ // логику подсмотрел
     if(!pauseFlag){
@@ -110,6 +127,11 @@ function main(){
     if(ballCenter.y+25 >= 600){
         ySpeed *=(-1);
         ball.style.top = `${ballYCord + ySpeed}px`;
+        lifes--;
+        document.querySelector('.lifes').innerHTML=lifes;
+        if(lifes==0){
+            endGameFlag=true;
+        }
     }else if(ballCenter.y-25 <= 0){
         ySpeed *=(-1);
         ball.style.top = `${ballYCord + ySpeed}px`;
@@ -177,20 +199,92 @@ function main(){
     ind3.style.top = `${contactPoint.y+30*directionY*(-1)}px`;
     ind3.style.left = `${contactPoint.x}px`;
 
-    let arr1 = document.elementsFromPoint(contactPoint.x+25*directionX*-1, contactPoint.y);
+    arr1 = document.elementsFromPoint(contactPoint.x+25*directionX*-1, contactPoint.y);
     for(let i of arr1){
-        if(i.className == 'brick') ySpeed*=(-1);
+        if(i.className == 'brick'){
+            ySpeed*=(-1);
+            actualScores+=10;
+            document.querySelector('.actualScores').innerHTML=actualScores;
+            arr1.length = 0;
+        }
     }
-    let arr2 = document.elementsFromPoint(contactPoint.x, contactPoint.y+25*directionY*-1);
+    arr2 = document.elementsFromPoint(contactPoint.x, contactPoint.y+25*directionY*-1);
     for(let i of arr2){
-        if(i.className == 'brick') xSpeed*=(-1);
+        if(i.className == 'brick'){
+            xSpeed*=(-1);
+            actualScores+=10;
+            document.querySelector('.actualScores').innerHTML=actualScores;
+            arr2.length = 0;
+        } 
     }
-    let arr3 = document.elementsFromPoint(contactPoint.x, contactPoint.y);
+    arr3 = document.elementsFromPoint(contactPoint.x, contactPoint.y);
     
-
     //изменение положения
     ball.style.top = `${ballYCord + ySpeed}px`;
     ball.style.left = `${ballXCord + xSpeed}px`;
+
+    //отсановка игры если нет жизней
+    if(lifes==0){
+        clearInterval(idForPause);
+        document.querySelector('.endGameScreen').style.left = `${100}px`;
+
+        //манипуляции с БД
+        playersDataBase[playername]=actualScores;
+        let winnersNameArray = [];
+        let winnersScoreArray=[];
+        let temp, tempName;
+        for(i in playersDataBase){
+            winnersNameArray.push(i);
+            winnersScoreArray.push(playersDataBase[i]);
+        }
+        for(let i=0;i<winnersScoreArray.length;i++){
+            for(let j=0;j<winnersScoreArray.length-i;j++){
+                if(winnersScoreArray[j]<winnersScoreArray[j+1]){
+                    temp=winnersScoreArray[j+1];
+                    tempName = winnersNameArray[j+1];
+
+                    winnersScoreArray[j+1]=winnersScoreArray[j];
+                    winnersNameArray[j+1]=winnersNameArray[j];
+
+                    winnersScoreArray[j]=temp;
+                    winnersNameArray[j]=tempName;
+                }
+            }
+        }
+        delete temp, tempName;
+        
+        //вывод списка лидеров на экран + подсветка нынешнего игрока
+        let firstTenCounter=1;
+        winnersTable = document.querySelector('.playersList');
+        for(let i=0;i<10;i++){
+            let ourIndex=winnersNameArray.indexOf(playername);
+            if(winnersNameArray.indexOf(playername)<10){
+                if(firstTenCounter==ourIndex+1){
+                    winnersTable.insertAdjacentHTML('beforeend',
+                    `<span class="winnersNames redOutline">${firstTenCounter}.&nbsp<span>${winnersNameArray[i]}:</span>&nbsp&nbsp&nbsp
+                    <span>${winnersScoreArray[i]}</span></span><br>`);
+                }else{
+                    winnersTable.insertAdjacentHTML('beforeend',
+                    `<span class="winnersNames">${firstTenCounter}.&nbsp<span>${winnersNameArray[i]}:</span>&nbsp&nbsp&nbsp
+                    <span>${winnersScoreArray[i]}</span></span><br>`);
+                }
+            }else{
+                if(i==8){
+                    winnersTable.insertAdjacentHTML('beforeend',`<span class="winnersNames withoutOutline">&nbsp<span>...</span>&nbsp&nbsp&nbsp
+                    </span><br>`);
+                }else if(i==9){
+                    winnersTable.insertAdjacentHTML('beforeend',
+                    `<span class="winnersNames redOutline">${ourIndex+1}.&nbsp<span>${winnersNameArray[ourIndex]}:</span>&nbsp&nbsp&nbsp
+                    <span>${winnersScoreArray[ourIndex]}</span></span><br>`);
+                }else{
+                    winnersTable.insertAdjacentHTML('beforeend',
+                    `<span class="winnersNames">${firstTenCounter}.&nbsp<span>${winnersNameArray[i]}:</span>&nbsp&nbsp&nbsp
+                    <span>${winnersScoreArray[i]}</span></span><br>`);
+                }
+            }
+            firstTenCounter++;
+        }
+    }
 }
 
 //==========================================================================================================================
@@ -214,24 +308,26 @@ player.addEventListener('submit', function(event){
     actualScores=0;
     screen1 = document.querySelector('.startScreen');
     screen1.style.left = `${-9999}px`;
-
 })
 
 
 document.addEventListener('keydown', plateMotion);
 document.addEventListener('keyup', stopPlateMotion);
 if(startGameFlag){
-    let idForPause = setInterval(main, 25);
+    idForPause = setInterval(main, 25);
 }
 
 document.addEventListener('keydown',function(event){
     if(event.code == 'Space'){
-        pauseFlag = !pauseFlag;
         event.preventDefault();
+        pauseFlag = !pauseFlag;
         if(pauseFlag){
             clearInterval(idForPause);
-        }else{
+            screen2.insertAdjacentHTML('beforeend',`<div class="pauseTitle">PAUSE</div>`);
+        }else if(!endGameFlag){
             idForPause = setInterval(main, 25);
+            document.querySelector('.pauseTitle').remove();
         }
     }
 })
+
